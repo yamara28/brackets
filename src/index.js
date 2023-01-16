45cf2fc5792381d640b456e0b640a7d2411d9190
Yegor Kozlov <101704236+yamara28@users.noreply.github.com>
2023-01-16 09:49:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const isOpenedBracket = (str) => {
+    return bracketsConfig.some(ar => ar[0].includes(str))
+  }
+
+  const isClosedBracket = (str) => {
+    return bracketsConfig.some(ar => ar[1].includes(str))
+  }
+
+  const isSameBrackets = (bracket1, bracket2) => {
+    return bracket1 === bracket2
+  }
+
+  const stack = [];
+
+  const array = Array.from(str);
+
+  for (let i = 0; i < array.length; i++) {
+    if (isOpenedBracket(array[i]) && !isClosedBracket(array[i])) {
+      stack.push(array[i])
+    }
+    else
+      if (isClosedBracket(array[i])) {
+        stack.push(array[i])
+
+        const getOpenedBracket = bracketsConfig.find(ar => ar[1].includes(array[i]))[0]
+        if (isSameBrackets(getOpenedBracket, stack[stack.length - 2])) { //-2 because closed bracket is added in stack
+          stack.pop(array[i])
+          stack.pop(array[i - 1])
+        }
+      }
+  }
+
+  if (stack.length === 0) {
+    return true
+  } else return false
 }
